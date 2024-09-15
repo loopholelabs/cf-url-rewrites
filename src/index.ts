f045d451d4d775d6fb7687ef9c06af88bd75584f
Shivansh Vij <shivanshvij@loopholelabs.io>
2024-09-15 14:40:28 -0700

    Adding no-index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+const robots = `
+User-agent: *
+Disallow: /
+`
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
+		if (url.pathname.endsWith('robots.txt')) {
+			return new Response(robots);
+		}
 		url.hostname = env.REWRITE_HOSTNAME;
 		return await fetch(url.toString(), request);
 	},
